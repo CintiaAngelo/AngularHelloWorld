93d696cc977c3294306fdb407fca1932bf8d24bc
Laboratório FIAP <labsfiap@fiap.com.br>
2025-03-26 22:03:21 -0300

    Primeiro Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FirstComponentComponent } from './components/first-component/first-component.component';
+import { NewComponentComponent } from './components/new-component/new-component.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,FirstComponentComponent, NewComponentComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'meu-app';
+  status : String ='sucesso'
 }
